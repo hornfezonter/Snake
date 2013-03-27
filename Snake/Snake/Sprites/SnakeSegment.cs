dd7d3bbacd59e0bca3f9906014adbc63e14bd354
hornfezonter <hornfezonter@foxmail.com>
2013-03-27 19:27:56 +0800

    SnakeSegment按方向Draw

--- Snake/Snake/Sprites/SnakeSegment.cs
+++ Snake/Snake/Sprites/SnakeSegment.cs
@@ -34,7 +34,32 @@ namespace Snake.Sprites
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            base.Draw(gameTime, spriteBatch);
+            Rectangle destRec = new Rectangle((int)(origin.X+position.X*displayWidth), (int)(origin.Y+position.Y*displayHeight), displayWidth,displayHeight);
+            Vector2 rotationOrigin = new Vector2((float)(origin.X+position.X*displayWidth + displayWidth * 0.5), (float)(origin.Y+position.Y*displayHeight + displayHeight * 0.5));
+            float rotation;
+
+            switch (direction)
+            {
+                case Direction.Up:
+                    rotation = 0;
+                    break;
+                case Direction.Left:
+                    rotation = 90;
+                    break;
+                case Direction.Down:
+                    rotation = 180;
+                    break;
+                case Direction.Right:
+                    rotation = 270;
+                    break;
+                default:
+                    rotation = 0;
+                    break;
+            }
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(img, destRec, null, Color.White, rotation, rotationOrigin, SpriteEffects.None, 0.5f);
+            spriteBatch.End();
         }
     }
 }
