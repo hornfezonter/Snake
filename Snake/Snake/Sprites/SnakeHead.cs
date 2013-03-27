94de76c24e33efec2ab3f2b622e98fca7974a085
hornfezonter <hornfezonter@foxmail.com>
2013-03-27 14:53:58 +0800

    SnakeHead类

---
+++ Snake/Snake/Sprites/SnakeHead.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Snake.Sprites
+{
+    public class SnakeHead:SnakeSegment
+    {
+        protected Direction nextDirection;
+
+         public SnakeHead() : base(){ }
+
+        public SnakeHead(Texture2D _img, Vector2 _origin, Point _position) : base(_img, _origin, _position) { }
+
+        public SnakeHead(Texture2D _img, Vector2 _origin, Point _position, Point _frameSize, Point _sheetSize) : base(_img, _origin, _position, _frameSize, _sheetSize) { }
+
+        public bool turn(Direction dir)
+        {
+            if (direction == Direction.Down || direction == Direction.Up)
+            {
+                if (dir == Direction.Left || dir == Direction.Right)
+                {
+                    nextDirection = dir;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (dir == Direction.Down || dir == Direction.Up)
+                {
+                    nextDirection = dir;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
--- Snake/Snake/Sprites/SnakeSegment.cs
+++ Snake/Snake/Sprites/SnakeSegment.cs
@@ -19,11 +19,22 @@ namespace Snake.Sprites
     {
         protected Direction direction;
 
-        public SnakeSegment() : base(){ }
+        public SnakeSegment() : base()
+        {
+            direction = Direction.Up;
+        }
 
-        public SnakeSegment(Texture2D _img, Vector2 _origin, Point _position) : base(_img, _origin, _position) { }
+        public SnakeSegment(Texture2D _img, Vector2 _origin, Point _position) : base(_img, _origin, _position) {
+            direction = Direction.Up;
+        }
 
-        public SnakeSegment(Texture2D _img, Vector2 _origin, Point _position, Point _frameSize, Point _sheetSize) : base(_img, _origin, _position, _frameSize, _sheetSize) { }
+        public SnakeSegment(Texture2D _img, Vector2 _origin, Point _position, Point _frameSize, Point _sheetSize) : base(_img, _origin, _position, _frameSize, _sheetSize) {
+            direction = Direction.Up;
+        }
 
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            base.Draw(gameTime, spriteBatch);
+        }
     }
 }
