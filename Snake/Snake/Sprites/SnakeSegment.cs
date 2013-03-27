92e8f6b9da856f9b09977e7d7817889144356b79
hornfezonter <hornfezonter@foxmail.com>
2013-03-27 14:25:30 +0800

    增加SnakeSegment类
    这是蛇的一节身体

---
+++ Snake/Snake/Sprites/SnakeSegment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Snake.Sprites
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Right,
+        Left
+    }
+
+    public class SnakeSegment:Tile
+    {
+        protected Direction direction;
+
+        public SnakeSegment() : base(){ }
+
+        public SnakeSegment(Texture2D _img, Vector2 _origin, Point _position) : base(_img, _origin, _position) { }
+
+        public SnakeSegment(Texture2D _img, Vector2 _origin, Point _position, Point _frameSize, Point _sheetSize) : base(_img, _origin, _position, _frameSize, _sheetSize) { }
+
+    }
+}
